1d6b6aeb8d02bacf13648ca5a9c4ba2bb020530a
Phivos Stylianides <stphivos@gmail.com>
2018-10-03 16:31:56 +0200

    Add merge scenarios support to scenarious base test and decorator

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -3,7 +3,7 @@ from scenarious.testing import scenario, ScenariousBaseTest
 from tests.test_scenarious import ActorTypeHandler
 
 
-class ScenariousFixtureWithTestClassTest(ScenariousBaseTest):
+class ScenariousFixtureWithTestClassTest(unittest.TestCase, ScenariousBaseTest):
 
     @scenario("""
       actors:
@@ -14,8 +14,26 @@ class ScenariousFixtureWithTestClassTest(ScenariousBaseTest):
           name: test2
           age: 22
     """, handlers=[ActorTypeHandler])
-    def test_fixture(self):
+    def test_fixture_simple(self):
         assert 2 == len(self.actors)
+        assert "test" == self.actors[0].name
+        assert "test2" == self.actors[1].name
+
+    scenario_a = """
+      actors:
+        - name: test
+          age: 20
+    """
+    scenario_b = """
+      actors:
+        - id: 2
+          name: test2
+          age: 22
+    """
+
+    @scenario(scenario_a, scenario_b, handlers=[ActorTypeHandler])
+    def test_fixture_with_merged_scenarios(self):
+        assert 2 == len(self.actors), self.actors
         assert "test" == self.actors[0].name
         assert "test2" == self.actors[1].name
 
